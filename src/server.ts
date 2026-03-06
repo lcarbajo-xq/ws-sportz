c119e857aad1ae4d1aab5714edecbb26a0eb1add
lcarbajo-xq <luis.carbajo.arguelles@gmail.com>
2026-03-06 12:27:24 +0100

    init project

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import cors from 'cors'
+import express from 'express'
+
+const app = express()
+const port = 3005
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (_req, res) => {
+  res.status(200).json({
+    success: true,
+    message: 'Servidor funcionando correctamente'
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Servidor ejecutándose en: http://localhost:${port}`)
+})
